a5bcc9360446c95b2f7cb3105af5328713cf458b
mfren114 <mathew.frenette@yahoo.com>
2024-09-19 15:46:30 -0700

    prep 4 modified

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+
+        {
+            Console.WriteLine("Enter a number (enter 0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The sum is: {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
