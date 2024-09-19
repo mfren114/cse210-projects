1a87981a68fccdebb8ffa5b78ef38b7ebc7d3ed4
mfren114 <mathew.frenette@yahoo.com>
2024-09-19 11:18:28 -0700

    Prep 3 modified

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        //Console.Write("What is the magic number? ");
+        //string number = Console.ReadLine();
+        //int magicNumber = int.Parse(number);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        
+        int userNumber = -1;
+
+        while (userNumber != magicNumber)
+        {
+
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+        
+
+            if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else  
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
