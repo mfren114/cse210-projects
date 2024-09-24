333c169e5ed027f37542cce61d207a25816e99e3
mfren114 <mathew.frenette@yahoo.com>
2024-09-24 10:21:28 -0700

    Week 2 practive program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Tesla";
+        job1._jobTitle = "Technician";
+        job1._startYear = 2014;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._company = "NASS";
+        job2._jobTitle = "Technician";
+        job2._startYear = 2011;
+        job2._endYear = 2014;
+
+        Resume resume = new Resume();
+        resume._name = "Mathew Frenette";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
+
     }
 }
