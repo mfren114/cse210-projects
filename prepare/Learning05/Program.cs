2f4c94b46eb128e2216c29a3b2b2d9bc3156c6ea
mfren114 <mathew.frenette@yahoo.com>
2024-10-15 10:09:23 -0700

    Learning 5 Update

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        //Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("Frenette", "Math");
+        //string summary = assignment.GetSummary();
+        Console.WriteLine(assignment.GetSummary());
+
+
+        MathAssignment mathAssignment = new MathAssignment("Frenette", "Math" ,"Section 7.3", "Problem 8-19");
+        //string homeWorkList = mathAssignment.GetHomeWorkList();
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
+
+
+        WritingAssignment writingAssignment = new WritingAssignment("Frenette", "English", "Welcome to Egypt");
+        //string getWritingInformation = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        
     }
 }
