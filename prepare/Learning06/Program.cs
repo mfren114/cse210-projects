ac8e4f95c4136e0beef9936af62f43b016618a1c
mfren114 <mathew.frenette@yahoo.com>
2024-10-22 14:19:30 -0700

    Learn 6 update

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        //Console.WriteLine("Hello Learning06 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        shapes.Add(square);
+        
+
+        Regtangle rectangle = new Regtangle("black", 5, 6);
+        shapes.Add(rectangle);
+        
+
+        Circle circle = new Circle("purple", 5);
+        shapes.Add(circle);
+        
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
